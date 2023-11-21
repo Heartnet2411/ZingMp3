26bce21f73d854cf034b74a57b455575640e6445
Heartnet <thuannguyen242135@gmail.com>
2023-11-21 21:32:18 +0700

    edit PlayMusic

--- ZingMp3/screens/PlayMusic.js
+++ ZingMp3/screens/PlayMusic.js
@@ -3,7 +3,7 @@ import { View, Text, Dimensions, StyleSheet, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Slider from '@react-native-community/slider';
 import { LinearGradient } from 'expo-linear-gradient';
-import { MaterialCommunityIcons, Feather, Octicons, FontAwesome5, AntDesign, Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons, Feather, Octicons, FontAwesome ,FontAwesome5, AntDesign, Ionicons } from '@expo/vector-icons';
 
 const PlayMusic = () => {
   return (
@@ -11,14 +11,14 @@ const PlayMusic = () => {
       <LinearGradient   colors={['#999', '#1e1e1e']} style={styles.container}>
         <Image style={styles.image} source={require('../assets/banner/cat-doi-noi-sau.jpg')} />
 
-        <View style={{justifyContent:'center', alignItems:'center', height: screenHeight*0.2}}>
+        <View style={{justifyContent:'center', alignItems:'center', height: screenHeight*0.22,}}>
           <View style={styles.name}>
-            <MaterialCommunityIcons name="share-outline" size={24} color="white" />
+            <FontAwesome name="share" size={30} color="#ccc" />
             <View style={styles.info}>
               <Text style={styles.songName}>Cắt đôi nỗi sầu</Text>
               <Text style={styles.author}>Tăng Duy Tân</Text>
             </View>
-            <Feather name="heart" size={24} color="white" />
+            <Feather name="heart" size={30} color="#ccc" />
           </View>
 
           <View style={styles.slider}>
@@ -38,19 +38,19 @@ const PlayMusic = () => {
         </View>
 
         <View style={styles.control}>
-          <FontAwesome5 name="random" size={24} color="white" />
-          <Ionicons name="ios-play-skip-back" size={24} color="white" />
-          <AntDesign name="playcircleo" size={24} color="white" />
-          <Ionicons name="play-skip-forward" size={24} color="white" />
-          <Feather name="repeat" size={24} color="white" />
+          <FontAwesome5 name="random" size={24} color="#ccc" />
+          <Ionicons name="ios-play-skip-back" size={35} color="white" />
+          <AntDesign name="playcircleo" size={70} color="white" />
+          <Ionicons name="play-skip-forward" size={35} color="white" />
+          <Feather name="repeat" size={24} color="#ccc" />
         </View>
 
         <View style={styles.feature}>
-          <Octicons name="comment" size={24} color="white" />
-          <MaterialCommunityIcons name="music-note-plus" size={24} color="white" />
+          <Octicons name="comment" size={30} color="#ccc" />
+          <MaterialCommunityIcons name="music-note-plus" size={30} color="#ccc" />
           <Text style={styles.quality}>320 Kbps</Text>
-          <Ionicons name="arrow-down-circle-outline" size={24} color="white" />
-          <MaterialCommunityIcons name="playlist-music-outline" size={24} color="white" />
+          <Ionicons name="arrow-down-circle-outline" size={30} color="#ccc" />
+          <MaterialCommunityIcons name="playlist-music-outline" size={30} color="#ccc" />
 
         </View>
       </LinearGradient>
@@ -65,8 +65,8 @@ const screenHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   container : {
-    width: screenWidth,
-    height: 'auto',
+    width: '100%',
+    height: '100%',
   },
 
   image: {
@@ -120,22 +120,30 @@ const styles = StyleSheet.create({
   control: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginTop: '10%',
+    marginTop: '5%',
     width: screenWidth*0.8,
     alignSelf: 'center',
-    height: screenHeight*0.08,
+    height: screenHeight*0.2,
+    alignItems: 'center',
+    flex: 1,
   },
   
   feature: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginTop: '10%',
     width: screenWidth*0.8,
     alignSelf: 'center',
-    height: screenHeight*0.08,
+    height: screenHeight*0.15,
+    alignItems: 'center',
   },
   quality: {
-    color: 'white',
+    color: '#ccc',
+    alignSelf: 'center',
+    backgroundColor: '#333',
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    fontWeight: 'bold',
   },
 
 });
